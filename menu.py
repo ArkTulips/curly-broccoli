cf50ee766979443f6c0450f8cc69df60aa39ad52
ArkTulips <tulipsark@gmail.com>
2025-09-11 02:17:59 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Page configuration
 st.set_page_config(
-    page_title="FinanceHub - Your Ultimate Financial Companion", 
+    page_title="Capital Compass - Your Ultimate Financial Companion", 
     layout="wide",
     page_icon="",
     initial_sidebar_state="collapsed"
