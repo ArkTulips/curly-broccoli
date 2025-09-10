18993e50d1e58dce54c008e2200e61fac003b0a3
ArkTulips <tulipsark@gmail.com>
2025-09-10 22:30:27 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 st.set_page_config(
     page_title="Finance Tools - Your Financial Companion", 
     layout="wide",
-    page_icon="💰",
+    page_icon="",
     initial_sidebar_state="collapsed"
 )
 
