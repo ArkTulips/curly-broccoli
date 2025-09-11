0f4828e06e5ae368b25f711c5796113eef6f4123
ArkTulips <tulipsark@gmail.com>
2025-09-11 12:54:20 +0530

    Update login.py

--- login.py
+++ login.py
@@ -30,12 +30,12 @@ password = st.text_input("Password", type="password")
 if st.button("Login"):
     if username in users and users[username] == password:
         st.success("✅ Login successful!")
-        st.markdown(
-            """
-            <meta http-equiv="refresh" content="0; url='https://menufin.streamlit.app/'" />
-            """,
-            unsafe_allow_html=True,
-        )
+          js = """
+        <script>
+        window.open("https://menufin.streamlit.app/", "_self");
+        </script>
+        """
+        st.markdown(js, unsafe_allow_html=True)
     else:
         st.error("❌ Invalid credentials")
 
